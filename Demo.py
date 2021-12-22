4847123f714210888f770ea24e5b7ddedecedd5e
Solo <shihundusolomon@gmail.com>
2021-12-22 11:17:35 +0300

    initial commit

---
+++ Demo.py
@@ -0,0 +1,11 @@
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+min_total = mins + dura
+end_min = min_total%60
+
+hrs_total = hour + (min_total//60)
+end_hrs = hrs_total%24
+
+print("END TIME: "+str(end_hrs)+":"+str(end_min))
